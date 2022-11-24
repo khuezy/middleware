d2f71f9060c2a7070b8a29d57efcf8eef5b707c3
Khue Nguyen <khuezy.nguyen@gmail.com>
2022-11-24 07:25:14 -0800

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  console.log('~~path: ', request.nextUrl.pathname)
+  console.log('~~url: ', request.url)
+  
   if (request.nextUrl.pathname === '/about') {
+    console.log('~~about redirect!')
     return NextResponse.redirect(new URL('/redirected', request.url))
   }
   if (request.nextUrl.pathname === '/another') {
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compress: false,
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
