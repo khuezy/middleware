c654ff74054b5b43a361a21fc67933ea282927a2
khuezy <khuezy.nguyen@gmail.com>
2022-12-09 11:44:32 -0800

    test ssr

--- pages/about2.tsx
+++ pages/about2.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from 'next'
 
-export const About2Page: NextPage = () => {
-  return <h1>About 2: No export {new Date().toISOString()}</h1>
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      time: new Date().toISOString()
+    }
+  }
+}
+export const About2Page: NextPage = (props: any) => {
+  return <h1>About 2: SSP {props.time}</h1>
 }
 
 export default About2Page
